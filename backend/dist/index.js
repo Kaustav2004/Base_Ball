803c3d09b3eed725dbac294b4b7dce42812a84bc
Kaustav Das <kaustavdas0228@gmail.com>
2025-02-06 12:43:55 +0530

    add backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -7,13 +7,22 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 // Initialize Firebase Admin SDK
-const firebase_adminsdk_json_1 = __importDefault(require("./firebase-adminsdk.json"));
+if (!process.env.FIREBASE_CONFIG) {
+    throw new Error("FIREBASE_CONFIG environment variable is not defined");
+}
+const firebaseConfig = JSON.parse(Buffer.from(process.env.FIREBASE_CONFIG, 'base64').toString('utf-8'));
+// import serviceAccount from './firebase-adminsdk.json';
 const allRoutes_1 = __importDefault(require("./Routes/allRoutes"));
 firebase_admin_1.default.initializeApp({
-    credential: firebase_admin_1.default.credential.cert(firebase_adminsdk_json_1.default),
-    databaseURL: 'https://baseball-2d081.firebaseio.com'
+    credential: firebase_admin_1.default.credential.cert(firebaseConfig),
 });
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+//   databaseURL: 'https://baseball-2d081.firebaseio.com'
+// });
 const app = (0, express_1.default)();
 // Middleware
 app.use((0, cors_1.default)());
@@ -22,8 +31,8 @@ app.use(body_parser_1.default.json());
 app.use("/api/v1", allRoutes_1.default);
 app.use("/", (req, res) => {
     res.status(404).json({
-        success: false,
-        message: 'Page not found'
+        success: true,
+        message: 'Home Page'
     });
 });
 // Start the server
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,30 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import admin from 'firebase-admin';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Initialize Firebase Admin SDK
-import serviceAccount from './firebase-adminsdk.json';
+if (!process.env.FIREBASE_CONFIG) {
+  throw new Error("FIREBASE_CONFIG environment variable is not defined");
+}
+
+const firebaseConfig = JSON.parse(
+  Buffer.from(process.env.FIREBASE_CONFIG, 'base64').toString('utf-8')
+);
+
+// import serviceAccount from './firebase-adminsdk.json';
 import allRoutes from './Routes/allRoutes';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  databaseURL: 'https://baseball-2d081.firebaseio.com'
+  credential: admin.credential.cert(firebaseConfig),
 });
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+//   databaseURL: 'https://baseball-2d081.firebaseio.com'
+// });
 
 const app = express();
 
